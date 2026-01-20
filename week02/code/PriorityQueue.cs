7770ba8a9a4bbc9ae65052aa91e60730458393e7
Paulo André <67445880+paulomath28@users.noreply.github.com>
2026-01-19 22:18:01 -0300

    Add files via upload

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -16,24 +16,29 @@
     }
 
     public string Dequeue()
+{
+    if (_queue.Count == 0)
     {
-        if (_queue.Count == 0) // Verify the queue is not empty
-        {
-            throw new InvalidOperationException("The queue is empty.");
-        }
-
-        // Find the index of the item with the highest priority to remove
-        var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
-        {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
-                highPriorityIndex = index;
-        }
-
-        // Remove and return the item with the highest priority
-        var value = _queue[highPriorityIndex].Value;
-        return value;
+        throw new InvalidOperationException("The line is empty.");
     }
+
+    int highPriorityIndex = 0;
+
+    for (int index = 1; index < _queue.Count; index++)
+    {
+        // Use '>' to preserve FIFO order in case of ties
+        if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
+        {
+            highPriorityIndex = index;
+        }
+    }
+
+    string value = _queue[highPriorityIndex].Value;
+    _queue.RemoveAt(highPriorityIndex);
+
+    return value;
+}
+
 
     // DO NOT MODIFY THE CODE IN THIS METHOD
     // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
