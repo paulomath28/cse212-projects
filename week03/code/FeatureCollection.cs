69e645e374e97d96f63e9c6bd3a2fed364435b06
Paulo André <67445880+paulomath28@users.noreply.github.com>
2026-01-23 22:49:12 -0300

    Add files via upload

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,15 @@
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public List<Feature> features { get; set; }
+}
+
+public class Feature
+{
+    public Properties properties { get; set; }
+}
+
+public class Properties
+{
+    public double? mag { get; set; }
+    public string place { get; set; }
 }
